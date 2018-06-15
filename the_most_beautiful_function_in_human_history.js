99b25032062b6e431a82cc8ff9982bc6355ca6d8
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 10:46:56 -0700

    Create the_most_beautiful_function_in_human_history.js

---
+++ the_most_beautiful_function_in_human_history.js
@@ -0,0 +1,12 @@
+//This is an exhibit dedicated to one of my favorite decision-making algorithms of all time.  
+//Please admire this function and appreciate the randomless of life.
+
+
+
+Math.random()
+
+
+
+
+//Check out http://javascript.info/ for an extremely well-written and thorough guide to JavaScript.
+//For an extremely beautiful poem on the randomness of life, check out the Book of Ecclesiastes.
