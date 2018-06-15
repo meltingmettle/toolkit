86990179d355663c6fc36f2457d5daebc7811854
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 09:24:23 -0700

    Create EntityGenerator.py

---
+++ EntityGenerator.py
@@ -0,0 +1,16 @@
+""" EXAMPLE """
+
+#@Input
+"""
+    @Attr1 int,
+    @Attr2 bit,
+    @Attr3 varchar(50),
+    @Attr4 varchar(50),
+"""
+#@Output
+"""
+    public int Attr1 { get; set; }
+    public int Attr2 { get; set; }
+    public String Attr3 { get; set; }
+    public String Attr4 { get; set; }
+"""
