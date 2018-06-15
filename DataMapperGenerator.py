5c3b395fbe3c3d920cac095c0f0f717b2c92a380
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 09:44:36 -0700

    Update DataMapperGenerator.py

--- DataMapperGenerator.py
+++ DataMapperGenerator.py
@@ -15,3 +15,16 @@
     Attr4 = sqlobject["Attr4"].toString(),
 """
 
+x = "@OID varchar(32),     @EquipmentID varchar(50),     @City varchar(50),   @FirstName varchar(100),     @LastName varchar(100),     @POName varchar(200),     @Timezone varchar(10),      @HomePhoneNumber varchar(20),     @CellPhoneNumber varchar(20),     @WorkPhoneNumber varchar(20),     @InputGroup varchar(100),     @Reason varchar(128),      @Comment varchar(1024),      @StartDate datetime,     @TerminationDate datetime, @StreetAddress varchar(50),     @State varchar(50),     @ZIP varchar(50), @EmergencyContactName varchar(200),     @EmergencyStreetAddress varchar(128),      @EmergencyCity varchar(50),      @EmergencyState varchar(50),      @EmergencyZip varchar(50),     @EmergencyPhone1 varchar(15),     @EmergencyPhone2 varchar(15)"
+
+def DataMapperGeneration(add_attribute_stored_procedure_parameters):
+    data = add_attribute_stored_procedure_parameters
+    attributelist = data.replace("'", "").replace(",", "").split("@")
+    for x in range(len(attributelist)):
+        attributelist[x] = attributelist[x].strip()
+        attributelist[x] = attributelist[x].split(" ")
+        if "varchar" in attributelist[x][1]:
+            attributelist[x][1] = "varchar"
+    return attributelist
+
+DataMapperGeneration(x)
