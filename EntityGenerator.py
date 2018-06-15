78c743f803804c9956888696e8b4bde51c209f91
Emmett Ling <emmettl@berkeley.edu>
2018-06-15 10:40:23 -0700

    Update EntityGenerator.py

--- EntityGenerator.py
+++ EntityGenerator.py
@@ -1,3 +1,9 @@
+
+#Toolkit!
+#Pass in the "insert" stored procedure, and DMG will output entity object implenetation. 
+#(SQL to C#)
+
+
 """ EXAMPLE """
 
 #@Input
@@ -14,3 +20,27 @@
     public String Attr3 { get; set; }
     public String Attr4 { get; set; }
 """
+
+#==================================IMPORTANT======================================
+#ATTRIBUTES WITH TYPES UNSUPPORTED IN C# OR OTHERWISE WITH SPECIAL NAMES WILL NEED TO BE ADDED OR CHECKED MANUALLY!
+
+#Varchar is default converted to "String".
+#Bit is default converted to "int"
+
+
+def EntityGenerator(add_attribute_stored_procedure_parameters):
+    data = add_attribute_stored_procedure_parameters.replace("'", "").replace(",", "").split("@")[1:]
+    def UnsupportedTypeRemoval(x):		#Helper function to manage data types which are unsupported in C#.  Not all-inclusive. 
+        if "varchar" in x:
+            return "string"
+        elif "bit" in x:
+            return "int"
+        else:
+            return x
+        
+    for x in range(len(data)):
+		item = data[x].strip().split(" ")
+        data[x] = "public " + UnsupportedTypeRemoval(item[1]) + " " + item[0] + "{get; set;}" 
+        print(data[x])
+
+#Enjoy!
